cdc97b0133e83d2da1e85530d4659796d0a5b878
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-02-24 17:37:26 -0500

    html css

---
+++ app/scripts/index.js
@@ -0,0 +1,25 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var handlebars = require('handlebars');
+
+
+
+
+
+
+
+
+
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
