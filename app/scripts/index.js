f317163980a2589f9c6b5aaf47b634793d8bef3d
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-02-25 08:54:12 -0500

    turn in

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -6,7 +6,7 @@ var handlebars = require('handlebars');
 
 
 
-
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=sli2r3htcrygifk6uyuicuzp&keywords=watch&includes=Images,Shop";
 
 
 
@@ -23,3 +23,26 @@ function fetchJSONP(url, callback) {
     script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
     document.body.appendChild(script);
 }
+
+var source = $("#all-cat-template").html();
+var template = handlebars.compile(source);
+
+function logData(data){
+  var objectData = data.results;
+  console.log(data.results);
+  objectData.forEach(function(value, index, thisArray){
+    var sortedData = {
+     title: thisArray[index].title,
+     price: thisArray[index].price,
+     currency_code: thisArray[index].currency_code,
+     manufacturer: thisArray[index].Shop.shop_name,
+     image: thisArray[index].Images[0].url_fullxfull,
+    }
+    $(".all-cat-imgs").append(template(sortedData));
+  });
+
+
+
+}
+
+fetchJSONP(url, logData);
