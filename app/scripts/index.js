9dc2bd5f3cfce2447b847182e65a60e01ba29e96
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-02-25 15:50:58 -0500

    extra stuff

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -27,22 +27,41 @@ function fetchJSONP(url, callback) {
 var source = $("#all-cat-template").html();
 var template = handlebars.compile(source);
 
+
 function logData(data){
+  console.log(data);
   var objectData = data.results;
-  console.log(data.results);
-  objectData.forEach(function(value, index, thisArray){
-    var sortedData = {
-     title: thisArray[index].title,
-     price: thisArray[index].price,
-     currency_code: thisArray[index].currency_code,
-     manufacturer: thisArray[index].Shop.shop_name,
-     image: thisArray[index].Images[0].url_fullxfull,
-    }
-    $(".all-cat-imgs").append(template(sortedData));
-  });
-
-
-
+  objectData.forEach(displayProductItem);
 }
+function displayProductItem(product){
+  var sortedData = {
+      title: product.title.substring(0, 27) + '...',
+      price: product.price,
+      currency_code: product.currency_code,
+      manufacturer: product.Shop.shop_name,
+      image: product.Images[0].url_fullxfull
+  };
+  console.log(product.title.substring(0, 32) + '...');
+  $('.js-search-results').append(template(sortedData));
+}
+
+// function logData(data){
+//   // console.log(data);
+//   var objectData = data.results;
+//   // console.log(data.results);
+//   objectData.forEach(function(value, index, thisArray){
+//     var sortedData = {
+//      title: thisArray[index].title,
+//      price: thisArray[index].price,
+//      currency_code: thisArray[index].currency_code,
+//      manufacturer: thisArray[index].Shop.shop_name,
+//      image: thisArray[index].Images[0].url_fullxfull,
+//     }
+//     $(".js-search-results").append(template(sortedData));
+//   });
+//
+//
+//
+// }
 
 fetchJSONP(url, logData);
